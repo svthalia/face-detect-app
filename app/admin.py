164a74f731a561ab3b6c3fca2ebf9a6e46558e95
Sébastiaan <se_bastiaan@outlook.com>
2020-10-25 20:02:08 +0100

    Add admin classes

---
+++ app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from app.models import Album, UserEncoding
+
+
+admin.site.register(Album)
+admin.site.register(UserEncoding)
---
+++ face_detection/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from face_detection.models import FaceEncoding
+
+
+admin.site.register(FaceEncoding)
